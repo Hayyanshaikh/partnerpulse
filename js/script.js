35f82478ccf38c7b896e80472f3112055cb5684c
hayyanshaikh <hayyanshaikh55@gmail.com>
2024-07-18 18:08:41 +0500

    Some Section of Home Page

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ $(function() {
     convertToOffcanvas();
   }).trigger('resize');
 
+  counter();
 });
 
 const convertToOffcanvas = () => {
@@ -13,4 +14,36 @@ const convertToOffcanvas = () => {
     $("#menu").show();
   }, 100);
   $(window).width() < 992 ? menu.addClass('offcanvas offcanvas-end shows') : menu.removeClass('offcanvas offcanvas-start');
+}
+
+const counter = () => {
+  const counters = $('.counter-number');
+  const speed = 400;
+
+  counters.each(function() {
+    const counter = $(this);
+    const target = counter.data('count');
+    let count = 0;
+
+    const updateCount = function() {
+      const increment = target / speed;
+      count += increment;
+
+      if (count < target) {
+        counter.text(Math.ceil(count));
+        setTimeout(updateCount, 1);
+      } else {
+        counter.text(target);
+      }
+    };
+
+    const observer = new IntersectionObserver(function(entries) {
+      if (entries[0].isIntersecting) {
+        updateCount();
+        observer.unobserve(counter[0]);
+      }
+    }, { threshold: 0.5 });
+
+    observer.observe(counter[0]);
+  });
 }
